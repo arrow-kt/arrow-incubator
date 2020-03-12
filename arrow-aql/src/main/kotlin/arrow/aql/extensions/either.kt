22558b944dc5054b3569ffeb346ee6b4fb2da4a4
Alisson Morais <alissonfpmorais@gmail.com>
2020-03-11 21:30:27 -0300

    :sparkles: add WhereOr instance to Either

--- arrow-aql/src/main/kotlin/arrow/aql/extensions/either.kt
+++ arrow-aql/src/main/kotlin/arrow/aql/extensions/either.kt
@@ -1,23 +1,17 @@
 package arrow.aql.extensions
 
-import arrow.aql.Select
-import arrow.aql.From
-import arrow.aql.GroupBy
-import arrow.aql.Count
-import arrow.aql.Sum
-import arrow.aql.OrderBy
-import arrow.aql.Union
-import arrow.aql.Max
-import arrow.aql.Min
+import arrow.aql.*
 import arrow.core.Either
 import arrow.core.EitherPartialOf
 import arrow.core.extensions.either.applicative.applicative
 import arrow.core.extensions.either.foldable.foldable
 import arrow.core.extensions.either.functor.functor
+import arrow.core.extensions.either.monadError.monadError
 import arrow.extension
 import arrow.typeclasses.Applicative
 import arrow.typeclasses.Foldable
 import arrow.typeclasses.Functor
+import arrow.typeclasses.MonadError
 
 @extension interface EitherSelect<L> : Select<EitherPartialOf<L>> {
   override fun functor(): Functor<EitherPartialOf<L>> = Either.functor()
@@ -26,6 +20,11 @@ import arrow.typeclasses.Functor
 @extension
 interface EitherFrom<L> : From<EitherPartialOf<L>> {
   override fun applicative(): Applicative<EitherPartialOf<L>> = Either.applicative()
+}
+
+@extension
+interface EitherWhereOr<L> : WhereOr<EitherPartialOf<L>, L> {
+  override fun monadError(): MonadError<EitherPartialOf<L>, L> = Either.monadError()
 }
 
 @extension
