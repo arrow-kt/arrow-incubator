4edef406298333b8f93722330868e58e2f8ae44b
Alberto Ballano <aballano@users.noreply.github.com>
2020-03-20 13:05:07 +0100

    Revert "Disable flaky tests (#74)"
    This reverts commit e5e832ab7ce05e86b27cf52f710729b72b24c579.
    
    # Conflicts:
    #	arrow-mtl-data/src/test/kotlin/arrow/mtl/test/EitherTTest.kt
    #	arrow-mtl-data/src/test/kotlin/arrow/mtl/test/KleisliTest.kt
    #	arrow-mtl-data/src/test/kotlin/arrow/mtl/test/OptionTTest.kt
    #	arrow-mtl-data/src/test/kotlin/arrow/mtl/test/WriterTTest.kt

--- arrow-mtl-data/src/test/kotlin/arrow/mtl/test/OptionTTest.kt
+++ arrow-mtl-data/src/test/kotlin/arrow/mtl/test/OptionTTest.kt
@@ -30,6 +30,13 @@ import arrow.core.test.laws.SemigroupKLaws
 import arrow.core.test.laws.TraverseFilterLaws
 import arrow.fx.IO
 import arrow.fx.test.eq.eqK
+import arrow.fx.extensions.io.concurrent.concurrent
+import arrow.fx.extensions.io.functor.functor
+import arrow.fx.extensions.io.monad.monad
+import arrow.fx.mtl.concurrent
+import arrow.fx.mtl.timer
+import arrow.fx.test.generators.genK
+import arrow.fx.test.laws.ConcurrentLaws
 import arrow.mtl.EitherT
 import arrow.mtl.Kleisli
 import arrow.mtl.KleisliPartialOf
@@ -44,6 +51,7 @@ import arrow.mtl.extensions.nested
 import arrow.mtl.extensions.optiont.applicative.applicative
 import arrow.mtl.extensions.optiont.divisible.divisible
 import arrow.mtl.extensions.optiont.eqK.eqK
+import arrow.mtl.extensions.optiont.functor.functor
 import arrow.mtl.extensions.optiont.functorFilter.functorFilter
 import arrow.mtl.extensions.optiont.monad.monad
 import arrow.mtl.extensions.optiont.monadPlus.monadPlus
@@ -81,15 +89,15 @@ class OptionTTest : UnitSpec() {
     val nestedEQK = OptionT.eqK(Id.eqK()).nested(OptionT.eqK(NonEmptyList.eqK()))
 
     testLaws(
-      // ConcurrentLaws.laws(
-      //   OptionT.concurrent(IO.concurrent()),
-      //   OptionT.timer(IO.concurrent()),
-      //   OptionT.functor(IO.functor()),
-      //   OptionT.applicative(IO.monad()),
-      //   OptionT.monad(IO.monad()),
-      //   OptionT.genK(IO.genK()),
-      //   ioEQK
-      // ),
+      ConcurrentLaws.laws(
+        OptionT.concurrent(IO.concurrent()),
+        OptionT.timer(IO.concurrent()),
+        OptionT.functor(IO.functor()),
+        OptionT.applicative(IO.monad()),
+        OptionT.monad(IO.monad()),
+        OptionT.genK(IO.genK()),
+        ioEQK
+      ),
 
       SemigroupKLaws.laws(
         OptionT.semigroupK(Option.monad()),
