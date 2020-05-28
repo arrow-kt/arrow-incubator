319e5461e54c2e8c2a4337da85d283d48e4fcc90
Alberto Ballano <aballano@users.noreply.github.com>
2020-05-28 16:18:53 +0200

    Replace AtomicRefW with Ref (#104)
    * Bring back ref instead of AtomicRef

--- arrow-fx-mtl/src/main/kotlin/arrow/fx/mtl/writert.kt
+++ arrow-fx-mtl/src/main/kotlin/arrow/fx/mtl/writert.kt
@@ -5,7 +5,6 @@ import arrow.core.Either
 import arrow.core.Tuple2
 import arrow.core.Tuple3
 import arrow.core.andThen
-import arrow.core.internal.AtomicRefW
 import arrow.extension
 import arrow.fx.IO
 import arrow.fx.RacePair
@@ -49,17 +48,18 @@ interface WriterTBracket<W, F> : Bracket<WriterTPartialOf<W, F>, Throwable>, Wri
     use: (A) -> WriterTOf<W, F, B>
   ): WriterT<W, F, B> = MM().run {
     MD().run {
-      val atomic: AtomicRefW<W> = AtomicRefW(empty())
-      WriterT(value().bracketCase(use = { wa ->
-        WriterT(wa.just()).flatMap(use).value()
-      }, release = { wa, exitCase ->
-        val r = release(wa.b, exitCase).value()
-        when (exitCase) {
-          is ExitCase.Completed -> r.flatMap { (l, _) -> later { atomic.value = l } }
-          else -> r.unit()
+      WriterT(Ref(empty()).flatMap { ref ->
+        value().bracketCase(use = { wa ->
+          WriterT(wa.just()).flatMap(use).value()
+        }, release = { wa, exitCase ->
+          val r = release(wa.b, exitCase).value()
+          when (exitCase) {
+            is ExitCase.Completed -> r.flatMap { (l, _) -> ref.set(l) }
+            else -> r.unit()
+          }
+        }).flatMap { (w, b) ->
+          ref.get().map { ww -> Tuple2(w.combine(ww), b) }
         }
-      }).map { (w, b) ->
-        Tuple2(w.combine(atomic.value), b)
       })
     }
   }
